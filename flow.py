95ab688269fcefefc5900627e3f44976e083fbe2
odelia0098 <odelia2012@yahoo.com>
2020-07-03 11:58:07 +0430

    Update flow.py

--- flow.py
+++ flow.py
@@ -33,27 +33,83 @@ class StaticFlowPusher(object):
         conn.close()
         return ret
   
-pusher = StaticFlowPusher('<insert_controller_ip')
+pusher = StaticFlowPusher('127.0.0.1')
   
 flow1 = {
-    'switch':"00:00:00:00:00:00:00:01",
-    "name":"flow_mod_1",
-    "cookie":"0",
+    "switch":"00:00:00:00:00:00:00:01",
+    "name":"flow1",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
     "priority":"32768",
-    "in_port":"1",
+    "in_port":"4",
     "active":"true",
-    "actions":"output=flood"
+    "actions":"output=1"
     }
   
 flow2 = {
-    'switch':"00:00:00:00:00:00:00:01",
-    "name":"flow_mod_2",
-    "cookie":"0",
+    "switch":"00:00:00:00:00:00:00:01",
+    "name":"flow2",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
+    "priority":"32768",
+    "in_port":"1",
+    "active":"true",
+    "actions":"output=2"
+    }
+    
+flow3 = {
+    "switch":"00:00:00:00:00:00:00:02",
+    "name":"flow3",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
+    "priority":"32768",
+    "in_port":"1",
+    "active":"true",
+    "actions":"output=3"
+    } 
+
+flow4 = {
+    "switch":"00:00:00:00:00:00:00:05",
+    "name":"flow4",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
+    "priority":"32768",
+    "in_port":"3",
+    "active":"true",
+    "actions":"output=2"
+    } 
+
+flow5 = {
+    "switch":"00:00:00:00:00:00:00:08",
+    "name":"flow5",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
     "priority":"32768",
     "in_port":"2",
     "active":"true",
-    "actions":"output=flood"
-    }
+    "actions":"output=3"
+    } 
+
+flow6 = {
+    "switch":"00:00:00:00:00:00:00:09",
+    "name":"flow6",
+    "eth_type":"0x0800",
+    "ipv4_src":"10.0.0.01",
+    "ipv4_dst":"10.0.0.05",
+    "priority":"32768",
+    "in_port":"3",
+    "active":"true",
+    "actions":"output=5"
+    }     
   
 pusher.set(flow1)
 pusher.set(flow2)
+pusher.set(flow3)
+pusher.set(flow4)
+pusher.set(flow5)
+pusher.set(flow6)
